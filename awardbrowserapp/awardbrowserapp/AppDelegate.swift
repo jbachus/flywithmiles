42672c8fd15d1198e36f2af3d5a876124ce1293e
Justin Bachus <justin@slcflightdeals.com>
2014-09-29 20:04:32 -0600

    Initial UI Design

---
+++ awardbrowserapp/awardbrowserapp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  awardbrowserapp
+//
+//  Created by Justin Bachus on 9/29/14.
+//  Copyright (c) 2014 Book it with Miles. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ awardbrowserapp/awardbrowserapp/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  awardbrowserapp
+//
+//  Created by Justin Bachus on 9/29/14.
+//  Copyright (c) 2014 Book it with Miles. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
