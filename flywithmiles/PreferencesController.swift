bb45ee152eb6b236274e10c3ab7d96ea306bc59a
Jonathan Chum <jchum@primoplace.com>
2014-12-08 21:13:13 -0500

    Added preferences controller

---
+++ flywithmiles/PreferencesController.swift
@@ -0,0 +1,13 @@
+//
+//  PreferencesController.swift
+//  flywithmiles
+//
+//  Created by Jonathan Chum on 10/27/14.
+//  Copyright (c) 2014 Jonathan Chum. All rights reserved.
+//
+
+import Foundation
+
+class PreferencesController {
+    
+}
