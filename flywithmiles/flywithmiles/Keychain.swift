8a2b010b776777118fc3607dff96a9e21fb4343b
Jonathan Chum <jchum@primoplace.com>
2014-12-12 21:49:18 -0500

    implemented class for access keychain, read/write dictionary

---
+++ flywithmiles/flywithmiles/Keychain.swift
@@ -0,0 +1,66 @@
+//
+//  Keychain.swift
+//  flywithmiles
+//
+//  Created by Jonathan Chum on 12/12/14.
+//  Copyright (c) 2014 Jonathan Chum. All rights reserved.
+//
+
+import Foundation
+
+// import UIKit
+import Security
+
+class Keychain {
+    
+    class func save(key: String, data: NSData) -> Bool {
+        let query = [
+            kSecClass as String       : kSecClassGenericPassword as String,
+            kSecAttrAccount as String : key,
+            kSecValueData as String   : data ]
+        
+        SecItemDelete(query as CFDictionaryRef)
+        
+        let status: OSStatus = SecItemAdd(query as CFDictionaryRef, nil)
+        
+        return status == noErr
+    }
+    
+    class func load(key: String) -> NSData? {
+        let query = [
+            kSecClass as String       : kSecClassGenericPassword,
+            kSecAttrAccount as String : key,
+            kSecReturnData as String  : kCFBooleanTrue,
+            kSecMatchLimit as String  : kSecMatchLimitOne ]
+        
+        var dataTypeRef :Unmanaged<AnyObject>?
+        
+        let status: OSStatus = SecItemCopyMatching(query, &dataTypeRef)
+        
+        if status == noErr {
+            return (dataTypeRef!.takeRetainedValue() as NSData)
+        } else {
+            return nil
+        }
+    }
+    
+    class func delete(key: String) -> Bool {
+        let query = [
+            kSecClass as String       : kSecClassGenericPassword,
+            kSecAttrAccount as String : key ]
+        
+        let status: OSStatus = SecItemDelete(query as CFDictionaryRef)
+        
+        return status == noErr
+    }
+    
+    
+    class func clear() -> Bool {
+        let query = [ kSecClass as String : kSecClassGenericPassword ]
+        
+        let status: OSStatus = SecItemDelete(query as CFDictionaryRef)
+        
+        return status == noErr
+    }
+    
+}
---
+++ flywithmiles/flywithmiles/PreferencesController.swift
@@ -0,0 +1,65 @@
+//
+//  PreferencesController.swift
+//  flywithmiles
+//
+//  Created by Jonathan Chum on 12/11/14.
+//  Copyright (c) 2014 Jonathan Chum. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+class PreferencesController: NSViewController {
+
+    @IBOutlet weak var airfranceUsername: NSTextField!
+    @IBOutlet weak var airfrancePassword: NSTextField!
+    
+    let service = "Flywithmiles"
+    let userAccount = "FlyWithMilesUser"
+    let key = "preferences"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let (dictionary, error) = Locksmith.loadData(forKey: key, inService: service, forUserAccount: userAccount)
+        
+        if let dictionary = dictionary {
+            println("Dictionary: \(dictionary)")
+        }
+        
+        if let error = error {
+            println("Error: \(error)")
+        }
+        
+        if (dictionary?["airfranceUsername"] != nil) {
+            self.airfranceUsername.stringValue = dictionary!["airfranceUsername"] as NSString
+        }
+        
+        if (dictionary?["airfrancePassword"] != nil) {
+            self.airfrancePassword.stringValue = dictionary!["airfrancePassword"] as NSString
+        }
+    }
+    
+    @IBAction func savePreferences(sender: AnyObject) {
+        Locksmith.deleteData(forKey: key, inService: service, forUserAccount: userAccount)
+        
+        Locksmith.saveData([
+            "airfranceUsername": self.airfranceUsername.stringValue,
+            "airfrancePassword" : self.airfrancePassword.stringValue
+        ], forKey: key, inService: service, forUserAccount: userAccount)
+    }
+    
+}
+
+extension String {
+    public var dataValue: NSData {
+        return dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)!
+    }
+}
+
+extension NSData {
+    public var stringValue: String {
+        return NSString(data: self, encoding: NSUTF8StringEncoding)!
+    }
+}
